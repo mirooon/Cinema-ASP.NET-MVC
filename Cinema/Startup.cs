337b2b32e41b46c0774ef9977a45e2c1308856ed
Michał Mirończuk <michal4010@wp.pl>
2017-10-27 15:19:24 +0200

    Add project files.

---
+++ Cinema/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cinema.Startup))]
+namespace Cinema
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
