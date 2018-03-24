9f44191e1684e2fe124753fe1e171c169d1b0333
Michał Mirończuk <michal4010@wp.pl>
2018-03-24 22:26:20 +0100

    Added Bundling

--- Cinema/App_Start/BundleConfig.cs
+++ Cinema/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Configuration;
+using System.Web;
 using System.Web.Optimization;
 
 namespace Cinema
@@ -11,6 +12,9 @@ namespace Cinema
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                         "~/Scripts/jquery-ui-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -19,13 +23,41 @@ namespace Cinema
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/bundles/bootstrappopper").Include(
+                      "~/Scripts/umd/popper.js",
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/site").Include(
+                     "~/Content/Site.css"));
+            bundles.Add(new StyleBundle("~/Content/jqueryuicss").Include(
+                     "~/Content/themes/base/jquery-ui.css"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+              "~/Content/themes/base/jquery.ui.core.css",
+              "~/Content/themes/base/jquery.ui.resizable.css",
+              "~/Content/themes/base/jquery.ui.selectable.css",
+              "~/Content/themes/base/jquery.ui.accordion.css",
+              "~/Content/themes/base/jquery.ui.autocomplete.css",
+              "~/Content/themes/base/jquery.ui.button.css",
+              "~/Content/themes/base/jquery.ui.dialog.css",
+              "~/Content/themes/base/jquery.ui.slider.css",
+              "~/Content/themes/base/jquery.ui.tabs.css",
+              "~/Content/themes/base/jquery.ui.datepicker.css",
+              "~/Content/themes/base/jquery.ui.progressbar.css",
+              "~/Content/themes/base/jquery.ui.theme.css"));
+
+#if DEBUG
+            BundleTable.EnableOptimizations = false;
+#else
+             BundleTable.EnableOptimizations = true;
+#endif
         }
+
+
     }
 }
---
+++ Cinema/App_Start/SiteKeys.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Cinema
+{
+    public class SiteKeys
+    {
+        public static string StyleVersion
+        {
+            get
+            {
+                return "<link href=\"{0}?v=" + ConfigurationManager.AppSettings["version"] + "\" rel=\"stylesheet\" type=\"text/css\"/>";
+            }
+        }
+        public static string ScriptVersion
+        {
+            get
+            {
+                return "<script src=\"{0}?v=" + ConfigurationManager.AppSettings["version"] + "\"></script>";
+            }
+        }
+    }
+}
